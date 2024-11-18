a7ac058746db7c1e5b0fa540b35a29bdf3e7ee99
Dorian <dorian.dep@gmail.com>
2024-11-18 16:38:58 +0100

    Add test_rotation_stiffness()

--- tests/test_StressStrainTensors.py
+++ tests/test_StressStrainTensors.py
@@ -175,6 +175,32 @@ class TestStrainStressTensors(unittest.TestCase):
         Tresca_th = np.array([1, 2, 0.0])
         np.testing.assert_array_equal(Tresca_stress, Tresca_th)
 
+    def test_rotation_stiffness(self, n_strain=100, n_ori=100):
+        """
+        Check that the two ways to compute stress from a rotated stiffness tensor are consistent.
+
+        Parameters
+        ----------
+        n_strain : int
+            Number of strain tensors to consider
+        n_ori : int
+            Number of random orientation to consider
+        """
+        matrix = np.random.random((n_strain, 3, 3))
+        eps = Tensors.StrainTensor(matrix).symmetricPart()  # Ensure that the strain is symmetric
+        ori = Rotation.random(n_ori)
+        sigma = Tensors.StressTensor.zeros((n_strain, n_ori))
+        for i in range(0, n_ori):
+            C_rotated = C * ori[i]
+            sigma[:, i] = C_rotated * eps
+
+        # Rotate stress and stress by their own
+        eps_rot = eps * ori
+        sigma_rot2 = C * eps_rot
+        sigma2 = sigma_rot2 * ori.inv()
+        sigma2 = sigma2[:, np.arange(n_ori), np.arange(n_ori), :, :]
+        np.testing.assert_almost_equal(sigma.matrix, sigma2.matrix)
+
 
 if __name__ == '__main__':
     unittest.main()
