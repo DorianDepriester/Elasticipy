08a97e2abdc98402df9c6137526b2cfc4a81f392
Dorian <dorian.dep@gmail.com>
2024-11-20 16:24:38 +0100

    Add setup.py

---
+++ src/Elasticipy/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Elasticipy',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'src/Elasticipy'},
+    url='https://github.com/DorianDepriester/Elasticipy',
+    license='MIT Licence',
+    author='Dorian Depriester',
+    author_email='dorian.depriester@ensam.eu',
+    description='Collection of tools to work on strain, stress and stiffness tensors, with plotting features'
+)
