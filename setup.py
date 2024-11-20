2ac42c035f8ad3ac44e2c9bd98dafd4527a7c695
Dorian Depriester <dorian@crans.org>
2024-11-20 20:53:51 +0100

    Update setup.py
    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Elasticipy',
-    version='1.0.0',
+    version='1.0.1',
     packages=[''],
     package_dir={'': 'src/Elasticipy'},
     url='https://github.com/DorianDepriester/Elasticipy',
