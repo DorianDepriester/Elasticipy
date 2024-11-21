8569d5317bd62c73d6789df72643dbc843705a7f
Dorian <dorian.dep@gmail.com>
2024-11-21 12:46:31 +0100

    Use sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ release = '1.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+   'sphinx_rtd_theme',
    'sphinx.ext.doctest',
    'sphinx.ext.autodoc',
    'sphinx.ext.autosummary',
@@ -33,7 +34,7 @@ language = 'english'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['../_static']
 numpydoc_class_members_toctree = False
 
