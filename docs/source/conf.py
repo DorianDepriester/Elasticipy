d6687d82392ab0ceb3140279a2f66b3204f57345
Dorian <dorian.dep@gmail.com>
2024-11-21 13:26:38 +0100

    Add viewcode and githubpages extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,9 @@ extensions = [
    'sphinx.ext.doctest',
    'sphinx.ext.autodoc',
    'sphinx.ext.autosummary',
-   'sphinx.ext.napoleon',]
+   'sphinx.ext.napoleon',
+   'sphinx.ext.viewcode',
+   'sphinx.ext.githubpages']
 templates_path = ['_templates']
 exclude_patterns = []
 
