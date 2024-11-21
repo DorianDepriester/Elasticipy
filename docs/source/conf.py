4124914522ac3d03600628b29bdaad7bde618662
Dorian <dorian.dep@gmail.com>
2024-11-21 11:08:07 +0100

    Fix wrong path to _static folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,6 @@ language = 'english'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
-
+html_static_path = ['../_static']
+numpydoc_class_members_toctree = False
 
