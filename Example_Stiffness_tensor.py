e40b76137662d12d13fd8eeaa441662a599fefa5
Dorian <dorian.dep@gmail.com>
2024-11-21 11:53:10 +0100

    Try removing numpydoc

--- Example_Stiffness_tensor.py
+++ Example_Stiffness_tensor.py
@@ -1,4 +1,4 @@
-from src.Elasticipy.FourthOrderTensor import tensorFromCrystalSymmetry
+from FourthOrderTensor import tensorFromCrystalSymmetry
 from scipy.spatial.transform import Rotation
 import matplotlib as mpl
 mpl.use('Qt5Agg')   # Ensure interactive plot
