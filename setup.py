fb25a0f3fe2117a491fcc50ef8e3e140b005b881
Dorian <dorian.dep@gmail.com>
2024-11-25 11:13:23 +0100

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Elasticipy',
-    version='1.1.0',
+    version='2.0.0',
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
     url='https://elasticipy.readthedocs.io/',
