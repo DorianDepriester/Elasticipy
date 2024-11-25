0006525ad4253049771aa519ba5c3d53717ec24e
Dorian <dorian.dep@gmail.com>
2024-11-25 12:00:55 +0100

    Update version with minor bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Elasticipy',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
     url='https://elasticipy.readthedocs.io/',
