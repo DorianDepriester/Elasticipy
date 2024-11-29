Fix wrong way to estimate the Christoffel Tensor Rename ChristoffelTensor to Christoffel_tensor Add Notes and reference, with table about the units to use

src/Elasticipy/SecondOrderTensor.py
@@ -81,7 +81,7 @@ class SecondOrderTensor:
         elif type(value) == self.__class__:
             self.matrix[index] = value.matrix
         else:
-            raise NotImplemented('The r.h.s must be either float, a ndarray or an object of class {}'.format(self.__class__))
+            raise NotImplementedError('The r.h.s must be either float, a ndarray or an object of class {}'.format(self.__class__))
 
     def __add__(self, other):
         if type(self) == type(other):
@@ -89,7 +89,7 @@ class SecondOrderTensor:
         elif isinstance(other, (int, float, np.ndarray)):
             return self.__class__(self.matrix + other)
         else:
-            raise NotImplemented('The element to add must be a number, a ndarray or of the same class.')
+            raise NotImplementedError('The element to add must be a number, a ndarray or of the same class.')
 
     def __sub__(self, other):
         if type(self) == type(other):
@@ -97,7 +97,7 @@ class SecondOrderTensor:
         elif isinstance(other, (int, float, np.ndarray)):
             return self.__class__(self.matrix - other)
         else:
-            raise NotImplemented('The element to subtract must be a number, a ndarray or of the same class.')
+            raise NotImplementedError('The element to subtract must be a number, a ndarray or of the same class.')
 
     @property
     def shape(self):