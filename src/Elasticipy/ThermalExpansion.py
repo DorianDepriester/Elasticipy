b70014fc929354f41bbd878388c95056345394a8
Dorian <dorian.dep@gmail.com>
2024-12-11 19:16:58 +0100

    Fix error in mul function

--- src/Elasticipy/ThermalExpansion.py
+++ src/Elasticipy/ThermalExpansion.py
@@ -1,17 +1,10 @@
 from Elasticipy.SecondOrderTensor import SymmetricSecondOrderTensor
-from StressStrainTensors import StrainTensor
+from Elasticipy.StressStrainTensors import StrainTensor
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 class ThermalExpansionTensor(SymmetricSecondOrderTensor):
     name = 'Thermal expansion tensor'
-
-    def __init__(self, matrix, symmetry='triclinic'):
-        matrix = np.asarray(matrix)
-        if matrix.shape == (3, 3):
-            super().__init__(matrix)
-            self.symmetry = symmetry
-        else:
-            raise ValueError('Matrix must have shape (3, 3).')
 
     def apply_temperature(self, T):
         if isinstance(T, (float, int)):
@@ -20,6 +13,29 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
             T = np.asarray(T)
             eps_mat = np.einsum('ij,...->...ij', self.matrix, T)
         return StrainTensor(eps_mat)
+
+    def __mul__(self, other):
+        if isinstance(other, Rotation):
+            return super().__mul__(other)
+        else:
+            other = np.asarray(other)
+            other_expanded = other[..., None, None]
+            other_with_eye = other_expanded * np.ones(3)
+            new_mat = self.matrix * other_with_eye
+            return StrainTensor(new_mat)
+
+    def matmul(self, other):
+        if isinstance(other, Rotation):
+            return super().matmul(other)
+        else:
+            other = np.asarray(other)
+            leading_shape = self.shape
+            leading_other_shape = other.shape
+            matrix_expanded = self.matrix.reshape(leading_shape + (1, 1, 3, 3))  # (m, n, 1, 1, 3, 3)
+            other_expanded = other.reshape((1, 1) + leading_other_shape + (1, 1))  # (1, 1, o, p, 1, 1)
+            new_mat = matrix_expanded * other_expanded
+            return StrainTensor(np.squeeze(new_mat))
+
 
     @classmethod
     def isotropic(cls, alpha):
@@ -30,11 +46,12 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
         ----------
         alpha : float
             Thermal expansion coefficient.
+
         Returns
         -------
         ThermalExpansionTensor
         """
-        return cls(np.eye(3)*alpha, symmetry='isotropic')
+        return cls(np.eye(3)*alpha)
 
     @classmethod
     def orthotropic(cls, alpha_1, alpha_2, alpha_3):
@@ -50,7 +67,7 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
         -------
         ThermalExpansionTensor
         """
-        return cls(np.diag([alpha_1, alpha_2, alpha_3]), symmetry='orthotropic')
+        return cls(np.diag([alpha_1, alpha_2, alpha_3]))
 
     @classmethod
     def orthorhombic(cls, *args):
@@ -104,7 +121,7 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
             raise ValueError('alpha_13 and alpha_12 cannot be used together.')
         else:
             raise ValueError('Either alpha_13 or alpha_12 must be provided.')
-        return cls(matrix, symmetry='monoclinic')
+        return cls(matrix)
 
 
     @classmethod
@@ -124,7 +141,7 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
         mat = [[alpha_11, alpha_12, alpha_13],
                [alpha_12, alpha_22, alpha_23],
                [alpha_13, alpha_23, alpha_33]]
-        return cls(np.diag(mat), symmetry='triclinic')
+        return cls(np.diag(mat))
 
     @classmethod
     def transverse_isotropic(cls, alpha_1, alpha_3):
@@ -142,4 +159,4 @@ class ThermalExpansionTensor(SymmetricSecondOrderTensor):
         -------
         ThermalExpansionTensor
         """
-        return cls(np.diag([alpha_1, alpha_1, alpha_3]), symmetry='transverse isotropic')
+        return cls(np.diag([alpha_1, alpha_1, alpha_3]))
