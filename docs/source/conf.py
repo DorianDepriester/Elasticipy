eb0935e1b74a725bdbecd13c8715b02bab9c3e58
Dorian <dorian.dep@gmail.com>
2025-01-14 15:21:43 +0100

    Add missing subfolder src in path to source files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,4 +49,4 @@ def linkcode_resolve(domain, info):
     if not info['module']:
         return None
     filename = info['module'].replace('.', '/')
-    return f"https://github.com/DorianDepriester/Elasticipy/blob/main/{filename}.py"
+    return f"https://github.com/DorianDepriester/Elasticipy/blob/main/src/{filename}.py"
