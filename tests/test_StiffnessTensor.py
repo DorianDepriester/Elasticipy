bfc3c541085db99cc5064e098e99483af14c98d8
Dorian <dorian.dep@gmail.com>
2025-01-21 09:41:49 +0100

    Also test invalid method name

--- tests/test_StiffnessTensor.py
+++ tests/test_StiffnessTensor.py
@@ -166,11 +166,16 @@ class TestStiffnessConstructor(unittest.TestCase):
             symmetry = row['symmetry']
             C = StiffnessTensor(matrix, symmetry=symmetry)
             C_rotated = C * rotations
-            for method in ('voigt', 'reuss', 'hill'):
-                Gavg = C.average(method).shear_modulus.mean(n_evals=10000)
-                assert row['G' + method] == approx(Gavg, rel=rel)
-                Gavg = C_rotated.average(method).shear_modulus.mean(n_evals=10000)
-                assert row['G' + method] == approx(Gavg, rel=rel)
+            for method in ('voigt', 'reuss', 'hill', 'dummy'):
+                if method == 'dummy':
+                    with self.assertRaises(NotImplementedError) as context:
+                        _ = C_rotated.average(method)
+                    self.assertEqual(str(context.exception), 'Only Voigt, Reus, and Hill are implemented.')
+                else:
+                    Gavg = C.average(method).shear_modulus.mean(n_evals=10000)
+                    assert row['G' + method] == approx(Gavg, rel=rel)
+                    Gavg = C_rotated.average(method).shear_modulus.mean(n_evals=10000)
+                    assert row['G' + method] == approx(Gavg, rel=rel)
 
     def test_stiffness_cubic(self):
         """Check that all symmetries in stiffness are well taken into account for cubic case"""
