9dc076e22c6e85b85b2e92511998fb62f5023a23
Dorian <dorian.dep@gmail.com>
2025-03-11 18:59:04 +0100

    Fix error when trying to compute S*stress

--- src/Elasticipy/tensors/elasticity.py
+++ src/Elasticipy/tensors/elasticity.py
@@ -90,7 +90,7 @@ class StiffnessTensor(SymmetricFourthOrderTensor):
 
     def __mul__(self, other):
         if isinstance(other, StrainTensor):
-            new_tensor = super().__mul__(other)
+            new_tensor = self.ddot(other)
             return StressTensor(new_tensor.matrix)
         elif isinstance(other, StressTensor):
             raise ValueError('You cannot multiply a stiffness tensor with a Stress tensor.')
@@ -1269,7 +1269,8 @@ class ComplianceTensor(StiffnessTensor):
 
     def __mul__(self, other):
         if isinstance(other, StressTensor):
-            return StrainTensor(self * other.matrix)
+            new_tensor = self.ddot(other)
+            return StrainTensor(new_tensor.matrix)
         elif isinstance(other, StrainTensor):
             raise ValueError('You cannot multiply a compliance tensor with Strain tensor.')
         else:
