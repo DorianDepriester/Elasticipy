97d280df93f2d9edfaa76f16e8fe6d9f7f19e0d8
Dorian <dorian.dep@gmail.com>
2025-11-03 13:38:01 +0100

    Export matrices to two different files, and check both

--- Examples/example_readwrite.py
+++ Examples/example_readwrite.py
@@ -2,10 +2,11 @@ from Elasticipy.tensors.elasticity import StiffnessTensor
 
 C = StiffnessTensor.from_MP("mp-1048")
 C.save_to_txt("TiNi.txt")
-C.save_to_txt("TiNi.txt", matrix_only=True)
+C.save_to_txt("TiNi-matrix.txt", matrix_only=True)
 
 C2 = StiffnessTensor.from_txt_file("TiNi.txt")
-Cu2= StiffnessTensor.from_txt_file("TiNi.txt")
-print(C2==Cu2)
+C3 = StiffnessTensor.from_txt_file("TiNi-matrix.txt")
+print(C==C2)
+print(C==C3)
 
 
