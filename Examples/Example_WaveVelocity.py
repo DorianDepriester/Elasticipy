64a743d36db9bd3d0635d6b3d6d91e9a1c78278b
Dorian <dorian.dep@gmail.com>
2025-11-12 13:07:27 +0100

    Use orthorhombic constructor instead of fromCrystalSymmetry

--- Examples/Example_WaveVelocity.py
+++ Examples/Example_WaveVelocity.py
@@ -1,9 +1,9 @@
 from Elasticipy.tensors.elasticity import StiffnessTensor
 from matplotlib import pyplot as plt
 
-C = StiffnessTensor.fromCrystalSymmetry(symmetry='orthorhombic', phase_name='forsterite',
-                                        C11=320, C12=68.2, C13=71.6,
-                                        C22=196.5, C23=76.8, C33=233.5, C44=64, C55=77, C66=78.7)
+C = StiffnessTensor.orthorhombic(phase_name='forsterite',
+                                C11=320, C12=68.2, C13=71.6,
+                                C22=196.5, C23=76.8, C33=233.5, C44=64, C55=77, C66=78.7)
 rho = 3.355
 
 cp, cs_fast, cs_slow = C.wave_velocity(rho)
