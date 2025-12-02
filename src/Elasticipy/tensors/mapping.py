eb886610cbf677881fd709b6434d3d15c163165c
Dorian <dorian.dep@gmail.com>
2025-12-02 16:55:16 +0100

    Add docstrings to mapping.py

--- src/Elasticipy/tensors/mapping.py
+++ src/Elasticipy/tensors/mapping.py
@@ -16,6 +16,17 @@ VOIGT_MAPPING_MATRIX_COMPLIANCE = [[1, 1, 1, 2, 2, 2],
                                   [2, 2, 2, 4, 4, 4]]
 
 class MappingConvention:
+    """
+    Generic class for defining the mapping convention to build a 4th-order tensor from a (6,6) matrix, and the mapping
+    convention to use for reciprocal tensor.
+
+    Attributes
+    ----------
+    matrix : numpy.ndarray
+        (6,6) matrix evidencing the coefficient between the 4-index and the 2-index notations
+    mapping_inverse : MappingConvention
+        Mapping convention to use for the reciprocal tensor
+    """
     matrix = np.array(KELVIN_MAPPING_MATRIX)
 
     @property
@@ -29,6 +40,43 @@ class VoigtMapping(MappingConvention):
     name = 'Voigt'
 
     def __init__(self, tensor='Stiffness'):
+        """
+        Create a Voigt mapping convention.
+
+        Parameters
+        ----------
+        tensor : str
+            It can be 'stiffness' or 'compliance'
+            Type of tensor we define. Depending on this, the mapping convention will change (see notes).
+
+        Notes
+        -----
+        For stiffness-like tensors (if ``tensor=stiffness``), the mapping matrix will be:
+
+        .. math::
+
+            \\begin{bmatrix}
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+                1 & 1 & 1 & 1 & 1 & 1\\\\
+            \end{bmatrix}
+
+        Conversely, for compliance-like tensors (if ``tensor=compliance``), the mapping matrix will be:
+
+        .. math::
+
+            \\begin{bmatrix}
+                1 & 1 & 1 & \\sqrt{2} & \\sqrt{2} & \\sqrt{2}\\\\
+                1 & 1 & 1 & \\sqrt{2} & \\sqrt{2} & \\sqrt{2}\\\\
+                1 & 1 & 1 & \\sqrt{2} & \\sqrt{2} & \\sqrt{2}\\\\
+                \\sqrt{2} & \\sqrt{2} & \\sqrt{2} & 2 & 2 & 2\\\\
+                \\sqrt{2} & \\sqrt{2} & \\sqrt{2} & 2 & 2 & 2\\\\
+                \\sqrt{2} & \\sqrt{2} & \\sqrt{2} & 2 & 2 & 2\\\\
+            \end{bmatrix}
+        """
         if tensor == 'Stiffness':
             self.matrix = np.ones((6,6))
             self.tensor_type = 'Stiffness'
