c879bd31285060ede3baf434a66a1669d260534b
Dorian <dorian.dep@gmail.com>
2026-01-16 17:07:39 +0100

    Define usual textures

--- src/elasticipy/crystal_textures.py
+++ src/elasticipy/crystal_textures.py
@@ -1,13 +1,38 @@
-from scipy.spatial.transform import Rotation
-from orix.quaternion import Rotation
+from orix.quaternion import Orientation
 import numpy as np
 from scipy.integrate import quad_vec
 
 class CrystalTexture:
-    pass
+    def __init__(self, orientation):
+        self.orientation = orientation
+
+    def __repr__(self):
+        return str(self.orientation.to_euler(degrees=True))
+
+    @classmethod
+    def Goss(cls):
+        o = Orientation.from_euler([0, 45, 0], degrees=True)
+        return CrystalTexture(o)
+
+    @classmethod
+    def Brass(cls):
+        o = Orientation.from_euler([35.26438968, 45, 0], degrees=True)
+        return CrystalTexture(o)
+
+    @classmethod
+    def Copper(cls):
+        o = Orientation.from_euler([90, 35.26438968, 45], degrees=True)
+        return CrystalTexture(o)
+
+    @classmethod
+    def S(cls):
+        o = Orientation.from_euler([58.97991646, 36.6992252, 63.43494882], degrees=True)
+        return CrystalTexture(o)
+
 
 class FibreTexture(CrystalTexture):
     def __init__(self, miller, axis):
+        super().__init__(None)
         self.miller = miller
         self.axis = axis
 
@@ -23,10 +48,10 @@ class FibreTexture(CrystalTexture):
         return row_0 + '\n' + row_1
 
     def mean_tensor(self, tensor):
-        ref_orient = Rotation.from_align_vectors(self.miller, self.axis)
+        ref_orient = Orientation.from_align_vectors(self.miller, self.axis)
         tensor_ref_orient = tensor * ref_orient
         def fun(theta):
-            rotation = Rotation.from_axes_angles(self.axis, theta)
+            rotation = Orientation.from_axes_angles(self.axis, theta)
             tensor_rotated = tensor_ref_orient * rotation
             return tensor_rotated.to_Kelvin()
         res, *_ = quad_vec(fun, 0, 2 * np.pi)
